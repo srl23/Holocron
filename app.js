78d4f878271b7f4b10258059756a6c7632ffff4d
srl23 <stephen.linfield@comcast.net>
2020-02-23 13:45:35 -0500

    search for planets works now, will be finishing up core functionality either tonight or tomorrow

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(() => {
-  $('form').on('submit', (event) => {
+  $('#characterForm').on('submit', (event) => {
 
     event.preventDefault();
 
@@ -23,5 +23,28 @@ $(() => {
         console.log('Not working, mate!');
       }
     )
-  })
+  });
+  $('#planetForm').on('submit', (event) => {
+    event.preventDefault();
+
+    const usersInput = $('input[type="text"]').val();
+
+    $.ajax({
+      url:'https://swapi.co/api/planets/?search=' + usersInput,
+    }).then(
+      (data) => {
+        console.log(data);
+        const $planetReturnData = data.results[0];
+        console.log($planetReturnData);
+        $('#name').text($planetReturnData.name);
+        $('#climate').text($planetReturnData.climate);
+        $('#diameter').text($planetReturnData.diameter);
+        $('#population').text($planetReturnData.population);
+        $('#gravity').text($planetReturnData.gravity);
+        },
+      (error) => {
+        console.log('Not working, mate!');
+      }
+    )
+  });
 });
